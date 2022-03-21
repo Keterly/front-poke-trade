c2dfed04d2ac2777fd629d67b95d73f9ebe4c345
Keterly <keterly.g@gmail.com>
2022-03-20 23:31:38 -0300

    alteracao versao node

--- poketrade/server.js
+++ poketrade/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express()
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/poketrade'));
 
 app.get('/*', (req, res) => {res.sendFile(__dirname + '/dist/poketrade/index.html')});
     
-app.listen(process.env.PORT || 8080);
+app.listen(port);
