bff055762fa310894c6d398438366003579b1816
Keterly <keterly.g@gmail.com>
2022-03-20 22:42:56 -0300

    mudanca arquivo server.js

--- poketrade/server.js
+++ poketrade/server.js
@@ -4,6 +4,6 @@ const app = express()
 
 app.use(express.static(__dirname + '/dist/poketrade'));
 
-app.get('/*', (req, res) => {res.sendFile(path.join(`${__dirname}/dist/poketrade/index.html`))});
+app.get('/*', (req, res) => {res.sendFile(__dirname + '/dist/poketrade/index.html')});
     
 app.listen(process.env.PORT || 8080);
